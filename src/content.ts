2a5cf2591f86a04f91ef312440bc2b71b9766d9a
mitou <xmitoux@gmail.com>
2024-01-21 19:54:24 +0900

    モデル選択欄を非表示にする機能を実装した
    close #3

--- src/content.ts
+++ src/content.ts
@@ -2,8 +2,10 @@ import { watchHistoryScripts } from './content-scripts/watch-history-scripts';
 import { deleteHistoryWithoutConfirm } from './content-scripts/delete-history-without-confirm';
 import { disableEnterGeneration } from './content-scripts/disable-enter-generation';
 import { saveHistoryShortcut } from './content-scripts/save-hisotry-shortcut';
+import { noModelSelector } from './content-scripts/no-model-selector';
 
 watchHistoryScripts();
 deleteHistoryWithoutConfirm();
 disableEnterGeneration();
 saveHistoryShortcut();
+noModelSelector();
