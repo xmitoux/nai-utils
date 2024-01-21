55a5a141ed0ebfff1012d0d78d3dd0b5c9061665
mitou <xmitoux@gmail.com>
2024-01-22 00:48:35 +0900

    プロンプト欄表示が1つのとき縮小機能でエラーが出るのを修正

--- src/content-scripts/shrink-prompt-area.ts
+++ src/content-scripts/shrink-prompt-area.ts
@@ -1,12 +1,15 @@
 export const shrinkPromptArea = () => {
     const proc = () => {
         const promptTextareaList = document.querySelectorAll<HTMLTextAreaElement>('textarea');
-        if (!promptTextareaList.length && promptTextareaList.length < 2) {
+        if (!promptTextareaList.length) {
             return;
         }
 
         promptTextareaList[0].style.height = '';
-        promptTextareaList[1].style.height = '';
+
+        if (promptTextareaList[1]) {
+            promptTextareaList[1].style.height = '';
+        }
     };
 
     new MutationObserver(proc).observe(document.body, { childList: true, subtree: true });
