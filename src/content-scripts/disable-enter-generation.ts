ffd97dc28ccdc259b15b56386622f2de3df27b7d
mitou <xmitoux@gmail.com>
2024-01-31 10:55:34 +0900

    `Ctrl + Enter`でどこでも生成機能を実装

--- src/content-scripts/disable-enter-generation.ts
+++ src/content-scripts/disable-enter-generation.ts
@@ -6,12 +6,23 @@ export const disableEnterGeneration = () => {
 
         const el = event.target as HTMLElement;
 
+        // プロンプト欄でのEnterキー生成を抑制
         if (
             el.tagName === 'TEXTAREA' &&
             !(event.ctrlKey || event.metaKey) &&
             event.key == 'Enter'
         ) {
             event.stopPropagation();
+            return;
+        }
+
+        // ページ内のどこでも Ctrl * Enter で生成
+        if ((event.ctrlKey || event.metaKey) && event.key == 'Enter') {
+            const generateButton = document.querySelector<HTMLButtonElement>(
+                'button:has(span):has(div)',
+            );
+            generateButton?.click();
+            return;
         }
     };
 
