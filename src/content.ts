795411c41ee55c16c3a4d0c0ddf1b22e07eb715d
mitou <xmitoux@gmail.com>
2024-01-31 10:30:42 +0900

    保存スクリプトのリネーム

--- src/content.ts
+++ src/content.ts
@@ -1,7 +1,7 @@
 import { watchHistoryScripts } from './content-scripts/watch-history-scripts';
 import { deleteHistoryWithoutConfirm } from './content-scripts/delete-history-without-confirm';
 import { disableEnterGeneration } from './content-scripts/disable-enter-generation';
-import { saveHistoryShortcut as saveImageScripts } from './content-scripts/save-hisotry-shortcut';
+import { saveImageScripts } from './content-scripts/saveImageScripts';
 import { noModelSelector } from './content-scripts/no-model-selector';
 import { shrinkPromptArea } from './content-scripts/shrink-prompt-area';
 
